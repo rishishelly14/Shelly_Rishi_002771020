d85a8f265c94462190ddf1ee547cdc49d1b524f3
rishishelly14 <114506649+rishishelly14@users.noreply.github.com>
2022-10-01 09:28:09 +0530

    Add files via upload
    Initial stages of assignment

---
+++ Assignment _1/src/model/EmployeeHistory.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author rishi
+ */
+public class EmployeeHistory {
+    
+    private ArrayList<Employees> history;
+    
+    public EmployeeHistory(){
+        
+        this.history = new ArrayList<Employees>();
+        
+        
+    }
+
+    public ArrayList<Employees> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<Employees> history) {
+        this.history = history;
+    }
+    
+    
+    
+}
---
+++ Assignment _1/src/model/Employees.java
@@ -0,0 +1,110 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author rishi
+ */
+public class Employees {
+    
+    private String name;
+    private int age;
+    private String gender;
+    private String StartDate;
+    private int level;
+    private String TeamInfo;
+    private String PositionTitle;
+    private String EmailAddress;
+    private int CellPhone;
+    private String EmployeeId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStartDate() {
+        return StartDate;
+    }
+
+    public void setStartDate(String StartDate) {
+        this.StartDate = StartDate;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getTeamInfo() {
+        return TeamInfo;
+    }
+
+    public void setTeamInfo(String TeamInfo) {
+        this.TeamInfo = TeamInfo;
+    }
+
+    public String getPositionTitle() {
+        return PositionTitle;
+    }
+
+    public void setPositionTitle(String PositionTitle) {
+        this.PositionTitle = PositionTitle;
+    }
+
+    public String getEmailAddress() {
+        return EmailAddress;
+    }
+
+    public void setEmailAddress(String EmailAddress) {
+        this.EmailAddress = EmailAddress;
+    }
+
+    public int getCellPhone() {
+        return CellPhone;
+    }
+
+    public void setCellPhone(int CellPhone) {
+        this.CellPhone = CellPhone;
+    }
+
+    public String getEmployeeId() {
+        return EmployeeId;
+    }
+
+    public void setEmployeeId(String EmployeeId) {
+        this.EmployeeId = EmployeeId;
+    }
+
+   
+    
+    
+    
+          
+    
+}
